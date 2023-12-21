6897839a6e006168103895718139bb57ae3c330f
omerrykb <omerryuksekbag@gmail.com>
2023-12-22 02:34:02 +0300

    Add error handling and input validation to the enUzunKelimeBul() function in app.js. This includes checking if the input is empty, displaying an error message if it is, and preventing the function from executing if the input is empty. Additionally, the function now displays the enlarged word and its length in a new div element. Finally, the code has been formatted and organized for readability.

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
 function enUzunKelimeBul() {
-    // Kullanıcıdan bir cümle al
-    let cumle = document.querySelector('#GirilecekCumle');
-    let inputDegeri = cumle.value;
-    
-    
-    if(inputDegeri==""){
-      return;
+  // Kullanıcıdan bir cümle al
+  let cumle = document.querySelector('#GirilecekCumle');
+  let inputDegeri = cumle.value;
+
+
+  // Cümleyi kelimelere ayır
+  let kelimeler = inputDegeri.split(" ");
+
+  // En uzun kelimeyi bulmak için bir değişken tanımlamakk
+  let enUzunKelime = "";
+
+  // forEach ile kelimeleri gez
+  kelimeler.forEach(function (kelime) {
+    // Eğer şu ana kadar bulduğunuz en uzun kelimenin uzunluğundan büyükse güncelleyin
+    if (kelime.length > enUzunKelime.length) {
+      enUzunKelime = kelime;
     }
+  });
 
-    // Cümleyi kelimelere ayır
-    let kelimeler = inputDegeri.split(" ");
-  
-    // En uzun kelimeyi bulmak için bir değişken tanımlamakk
-    let enUzunKelime = "";
-  
-    // forEach ile kelimeleri gez
-    kelimeler.forEach(function(kelime) {
-      // Eğer şu ana kadar bulduğunuz en uzun kelimenin uzunluğundan büyükse güncelleyin
-      if (kelime.length > enUzunKelime.length) {
-        enUzunKelime = kelime;
-      }
-    });
+  //error bloğunu çalıştır
+  let errorDiv = document.querySelector(".error");
 
-    // En uzun kelimeyi bir div içine ekleyin
-    let sonucListesi = document.querySelector('#sonucListesi');
-    let yeniSonucDiv = document.createElement('div');
-    yeniSonucDiv.innerHTML = `<li>Cümledeki en uzun kelime:<span class="blueSpan">${enUzunKelime}</span>ve<span class="blueSpan">${enUzunKelime.length}</span>karakterden oluşmaktadır.`;
-    sonucListesi.appendChild(yeniSonucDiv);
-
-    //inputtaki yazıyı temizle
-    cumle.value=""
-    
-
-
-    if(sonucListesi.innerHTML===""){
-      sonucListesi.style.display="none";
-    }
-    else{
-      sonucListesi.style.display="block";
-    }
+  //input boşsa eğer hata ver..
+  if (inputDegeri == "") {
+    errorDiv.innerHTML = `Lütfen Bir Cümle Giriniz!`;
+    yeniSonucDiv.innerHTML = null
+  }
+  else {
+    errorDiv.innerHTML = null;
   }
 
-  
+  // En uzun kelimeyi her seferinde yeni div oluşturarak listeye ekle
+  let sonucListesi = document.querySelector('#sonucListesi');
+  let yeniSonucDiv = document.createElement('div');
+  yeniSonucDiv.innerHTML = `<li>Cümledeki en uzun kelime:<span class="blueSpan">${enUzunKelime}</span> ve<span class="blueSpan">${enUzunKelime.length}</span>karakterden oluşmaktadır.`;
+  sonucListesi.appendChild(yeniSonucDiv);
+
+
+
+  if (yeniSonucDiv !== "") {
+    sonucListesi.style.display = "block"
+  }
+  else {
+    sonucListesi.style.display = "none"
+  }
+
+  //inputtaki yazıyı temizle
+  cumle.value = ""
+
+  //scroll en altta kalsın
+  sonucListesi.scrollTop = sonucListesi.scrollHeight
+
+}
+
+//end of file
